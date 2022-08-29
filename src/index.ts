31fe95898d93607ccf23ebd2cd4e9bb05bc9f5cd
Slavik6661 <Slavik0.75@mail.ru>
2022-08-29 14:58:26 +0300

    satart

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+let path = require("path");
+
+app.use(express.static("data.js"));
+app.use("/static", express.static(path.join(__dirname, "../static")));
+
+app.use("/", (req, res) => {
+  res.sendFile(__dirname + "/views/htmlStore/subwayStore.html");
+});
+
+app.listen(3000, () => console.log("listening on localhost:3000"));
